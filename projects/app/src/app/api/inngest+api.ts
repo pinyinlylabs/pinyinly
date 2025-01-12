a3d1cb4ea72aa450b705583a508057a0247d9bd4
Bradley Ayers <bradley.ayers@gmail.com>
2025-01-12 21:35:47 +0800

    Swap inngest to using Bun integration

--- projects/app/src/app/api/inngest+api.ts
+++ projects/app/src/app/api/inngest+api.ts
@@ -1,7 +1,11 @@
 import { functions, inngest } from "@/server/lib/inngest";
-import { serve } from "inngest/next";
+import { serve } from "inngest/bun";
 
-export const { GET, POST, PUT } = serve({
+const handler = serve({
   client: inngest,
   functions,
 });
+
+export const GET = handler;
+export const PUT = handler;
+export const POST = handler;
