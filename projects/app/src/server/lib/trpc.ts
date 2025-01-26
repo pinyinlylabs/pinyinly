3778c6aa3d831137a9c62a669bd4f8dcbadbb769
Bradley Ayers <bradley.ayers@gmail.com>
2025-01-26 21:46:18 +1100

    Don't send server error messages to the client

--- projects/app/src/server/lib/trpc.ts
+++ projects/app/src/server/lib/trpc.ts
@@ -4,7 +4,16 @@ import { Context } from "./trpcContext";
 
 // Avoid exporting the entire t-object since it's not very descriptive. For
 // instance, the use of a t variable is common in i18n libraries.
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  errorFormatter: ({ shape, error }) => {
+    return {
+      ...shape,
+      // Don't expose server-side error messages to the client as they might
+      // contain sensitive information.
+      message: __DEV__ ? error.message : `An error occurred`,
+    };
+  },
+});
 
 const sentryMiddleware = t.middleware(
   trpcMiddleware({
