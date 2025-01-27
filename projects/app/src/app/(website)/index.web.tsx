6dbf627c1264a07c7fbb5665b185d67f60df7445
Bradley Ayers <bradley.ayers@gmail.com>
2025-01-28 01:07:11 +1100

    Add more website skeleton

--- projects/app/src/app/(website)/index.web.tsx
+++ projects/app/src/app/(website)/index.web.tsx
@@ -1,22 +1,13 @@
-import { Image } from "expo-image";
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function WebsitePage() {
   return (
     <View className="items-center">
-      <Text className="mt-4 text-2xl text-text">haohaohow</Text>
-      <Image
-        source={require(`@/assets/adaptive-icon.png`)}
-        style={[{ flexShrink: 1, width: 100, height: 100 }]}
-      />
-      <View className="h-[100px]"></View>
-      <Link href={`/company`} className="text-text">
-        Company
-      </Link>
-      <Link href={`/dashboard`} className="text-text">
-        Dashboard
-      </Link>
+      <Text className="text-4xl font-bold text-text">
+        Teach yourself Chinese
+      </Text>
+
+      <View className="h-[400px]"></View>
     </View>
   );
 }
