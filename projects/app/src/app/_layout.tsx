b911e163fec4028a7df0ca361b3985dad4911abb
Bradley Ayers <bradley.ayers@gmail.com>
2025-02-05 13:18:42 +1100

    Fix document title on web

--- projects/app/src/app/_layout.tsx
+++ projects/app/src/app/_layout.tsx
@@ -38,6 +38,7 @@ cssInterop(Image, {
 cssInterop(Animated.View, { className: `style` });
 
 import * as AppleAuthentication from "expo-apple-authentication";
+import Head from "expo-router/head";
 
 cssInterop(AppleAuthentication.AppleAuthenticationButton, {
   className: `style`,
@@ -78,6 +79,10 @@ function RootLayout() {
               } satisfies ReactNavigationTheme
             }
           >
+            <Head>
+              <title>haohaohow - Teach yourself Chinese</title>
+            </Head>
+
             <View
               className={`${
                 // This is the native equivalent of adding a class to the body
