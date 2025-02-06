d7a9678b0ea15421745fd429acf9f003a4c79c1e
Bradley Ayers <bradley.ayers@gmail.com>
2025-02-06 17:46:03 +1100

    Fix modal presentation on web
    See https://github.com/react-navigation/react-navigation/pull/12428

--- projects/app/src/app/(sidenav)/learn/_layout.tsx
+++ projects/app/src/app/(sidenav)/learn/_layout.tsx
@@ -1,19 +1,22 @@
+import { PropsOf } from "@/client/ui/types";
 import { Stack } from "expo-router";
 
 export const unstable_settings = {
   initialRouteName: `index`,
 };
 
+const modalScreenOptions = {
+  animation: `slide_from_bottom`,
+  presentation: `fullScreenModal`,
+} satisfies PropsOf<typeof Stack.Screen>[`options`];
+
 export default function LearnLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen
-        name="reviews"
-        options={{
-          animation: `fade`,
-          presentation: `fullScreenModal`,
-        }}
-      />
+      <Stack.Screen name="reviews" options={modalScreenOptions} />
+      <Stack.Screen name="radicals" options={modalScreenOptions} />
+      <Stack.Screen name="hsk1" options={modalScreenOptions} />
+      <Stack.Screen name="connections" options={modalScreenOptions} />
     </Stack>
   );
 }
