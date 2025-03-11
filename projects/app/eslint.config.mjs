8bb64786c05f8843fd317216c50db46be0d9af26
Bradley Ayers <bradley.ayers@gmail.com>
2025-03-12 10:57:16 +1100

    enable unicorn/prefer-string-replace-all

--- projects/app/eslint.config.mjs
+++ projects/app/eslint.config.mjs
@@ -246,7 +246,6 @@ export default tseslint.config(
       "unicorn/prefer-global-this": `off`, // TODO evaluate
       "unicorn/no-object-as-default-parameter": `off`, // TODO evaluate
       "unicorn/no-anonymous-default-export": `off`, // TODO evaluate
-      "unicorn/prefer-string-replace-all": `off`, // TODO evaluate
       "unicorn/no-unreadable-array-destructuring": `off`, // TODO evaluate
       "unicorn/prefer-array-flat-map": `off`, // TODO evaluate
       "unicorn/prefer-top-level-await": `off`, // TODO evaluate
