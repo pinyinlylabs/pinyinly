ac66afb3cf9eb931b9511aadc58cf287568fcadc
Bradley Ayers <bradley.ayers@gmail.com>
2025-03-17 01:37:31 +1100

    restore sentry for api routes

--- projects/app/api/index.cjs
+++ projects/app/api/index.cjs
@@ -1,29 +1,16 @@
-// const Sentry = require(`@sentry/node`);
-// const { captureConsoleIntegration } = require(`@sentry/core`);
+const Sentry = require(`@sentry/node`);
+const { captureConsoleIntegration } = require(`@sentry/core`);
 
-// Sentry.init({
-//   dsn: process.env.SENTRY_DSN, // Must be provided at runtime.
-//   integrations: [captureConsoleIntegration()],
-//   // tracesSampleRate: 1, // Keep in sync with the other Sentry.init()
-//   // profilesSampleRate: 1,
-//   debug: true,
-// });
+Sentry.init({
+  dsn: process.env.SENTRY_DSN, // Must be provided at runtime.
+  integrations: [captureConsoleIntegration()],
+  tracesSampleRate: 1, // Keep in sync with the other Sentry.init()
+  profilesSampleRate: 1,
+});
 
 const { createRequestHandler } = require(`@expo/server/adapter/vercel`);
 
-const handler = createRequestHandler({
+module.exports = createRequestHandler({
   // build: require(`path`).join(__dirname, `../dist/.cache/vercel-expo/server`),
   build: require(`path`).join(__dirname, `../dist/vercel/server`),
 });
-
-/** @type {import('@expo/server/adapter/vercel').RequestHandler} */
-// eslint-disable-next-line unicorn/no-anonymous-default-export
-module.exports = async (req, res) => {
-  try {
-    await handler(req, res);
-  } catch (error) {
-    console.error(`error during @expo/server createRequestHandler`, error);
-    res.writeHead(500, { "Content-Type": `text/plain` });
-    res.end(`Internal Server Error`);
-  }
-};
