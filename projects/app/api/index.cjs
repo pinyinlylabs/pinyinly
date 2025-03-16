ada3906f61729202af6dfe0441300e6bb746b07b
Bradley Ayers <bradley.ayers@gmail.com>
2025-03-17 01:13:40 +1100

    debugging

--- projects/app/api/index.cjs
+++ projects/app/api/index.cjs
@@ -11,15 +11,15 @@
 
 const { createRequestHandler } = require(`@expo/server/adapter/vercel`);
 
-const handler = createRequestHandler({
-  // build: require(`path`).join(__dirname, `../dist/.cache/vercel-expo/server`),
-  build: require(`path`).join(__dirname, `../dist/vercel/server`),
-});
-
 /** @type {import('@expo/server/adapter/vercel').RequestHandler} */
 // eslint-disable-next-line unicorn/no-anonymous-default-export
 module.exports = async (req, res) => {
   try {
+    const handler = createRequestHandler({
+      // build: require(`path`).join(__dirname, `../dist/.cache/vercel-expo/server`),
+      build: require(`path`).join(__dirname, `../dist/vercel/server`),
+    });
+
     await handler(req, res);
   } catch (error) {
     console.error(`error during @expo/server createRequestHandler`, error);
