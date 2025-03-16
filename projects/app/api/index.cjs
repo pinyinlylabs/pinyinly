6c444a248e8a3f1b258d053d30b687a6928b3a69
Bradley Ayers <bradley.ayers@gmail.com>
2025-03-17 01:30:37 +1100

    debugging

--- projects/app/api/index.cjs
+++ projects/app/api/index.cjs
@@ -11,20 +11,19 @@
 
 const { createRequestHandler } = require(`@expo/server/adapter/vercel`);
 
+const handler = createRequestHandler({
+  // build: require(`path`).join(__dirname, `../dist/.cache/vercel-expo/server`),
+  build: require(`path`).join(__dirname, `../dist/vercel/server`),
+});
+
 /** @type {import('@expo/server/adapter/vercel').RequestHandler} */
 // eslint-disable-next-line unicorn/no-anonymous-default-export
 module.exports = async (req, res) => {
-  // try {
-  //   const handler = createRequestHandler({
-  //     // build: require(`path`).join(__dirname, `../dist/.cache/vercel-expo/server`),
-  //     build: require(`path`).join(__dirname, `../dist/vercel/server`),
-  //   });
-
-  //   await handler(req, res);
-  // } catch (error) {
-  // console.error(`ok method`, error);
-  // console.error(`error during @expo/server createRequestHandler`, error);
-  res.writeHead(200, { "Content-Type": `text/plain` });
-  res.end(`ok working`);
-  // }
+  try {
+    await handler(req, res);
+  } catch (error) {
+    console.error(`error during @expo/server createRequestHandler`, error);
+    res.writeHead(500, { "Content-Type": `text/plain` });
+    res.end(`Internal Server Error`);
+  }
 };
