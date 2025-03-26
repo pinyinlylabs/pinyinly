7bde3b1640aba9ff84dff6c2c7b039b01626ff70
Bradley Ayers <bradley.ayers@gmail.com>
2025-03-26 14:21:55 +0900

    Avoid conflicting hanzi in quiz choices

--- projects/app/test/util/rizzleHelpers.ts
+++ projects/app/test/util/rizzleHelpers.ts
@@ -2,7 +2,7 @@ import { ReplicacheOptions, TEST_LICENSE_KEY } from "replicache";
 
 let _testReplicacheNameId = 0;
 /**
- * Create a @see ReplicacheOptions object suitable for testing.
+ * Create a {@link ReplicacheOptions} object suitable for testing.
  *
  * @param name The name of the database. By default it will be unique for each
  * call but can a value can be passed in if you want to share a database.
