db6f3174e264a5a7b41268623deadcc532c37809
Bradley Ayers <bradley.ayers@gmail.com>
2025-04-21 19:00:13 +0900

    fix not centered quiz "Loading" text

--- projects/app/src/app/(sidenav)/learn/reviews.tsx
+++ projects/app/src/app/(sidenav)/learn/reviews.tsx
@@ -1,30 +1,13 @@
 import { ErrorBoundary } from "@/client/ui/ErrorBoundary";
 import { QuizDeck } from "@/client/ui/QuizDeck";
-import { useCallback, useState } from "react";
-import { Text, View } from "react-native";
-import Animated, { FadeIn } from "react-native-reanimated";
-import { useTimeout } from "usehooks-ts";
+import { View } from "react-native";
 
 export default function ReviewsPage() {
-  const [visible, setVisible] = useState(false);
-
-  const show = useCallback(() => {
-    setVisible(true);
-  }, []);
-
-  useTimeout(show, 500);
-
   return (
     <View className="flex-1 items-center bg-background pt-safe-offset-[20px]">
-      {visible ? (
-        <ErrorBoundary>
-          <QuizDeck className="h-full w-full" />
-        </ErrorBoundary>
-      ) : (
-        <Animated.View entering={FadeIn} className="my-auto">
-          <Text className="font-karla text-lg text-primary-10">Loading</Text>
-        </Animated.View>
-      )}
+      <ErrorBoundary>
+        <QuizDeck className="h-full w-full" />
+      </ErrorBoundary>
     </View>
   );
 }
