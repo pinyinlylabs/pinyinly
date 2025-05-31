a93d76c79168b8243cfeda63c869d6c702342548
Bradley Ayers <bradley.ayers@gmail.com>
2025-05-31 19:45:21 +1000

    iterate hanzi to pinyin quiz

--- projects/app/src/client/ui/PinyinOptionButton.tsx
+++ projects/app/src/client/ui/PinyinOptionButton.tsx
@@ -4,21 +4,30 @@ import { RectButton2 } from "./RectButton2";
 import type { PropsOf } from "./types";
 
 interface PinyinOptionButtonProps
-  extends Omit<PropsOf<typeof RectButton2>, `variant` | `children`> {
+  extends Omit<
+    PropsOf<typeof RectButton2>,
+    `variant` | `children` | `onPress`
+  > {
   pinyin: string;
   shortcutKey: string;
+  onPress?: (pinyin: string) => void;
 }
 
 export function PinyinOptionButton({
   pinyin,
   shortcutKey,
   className,
+  onPress,
   ...props
 }: PinyinOptionButtonProps) {
+  const handlePress = () => {
+    onPress?.(pinyin);
+  };
   return (
     <RectButton2
       variant="option"
       className={buttonClass({ className })}
+      onPress={handlePress}
       {...props}
     >
       <Text className="hhh-text-button-option">{pinyin}</Text>
