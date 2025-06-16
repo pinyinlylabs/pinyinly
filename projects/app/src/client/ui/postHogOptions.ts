a4def21e5eaa58774c7d39ad9c320ee514f8c511
Bradley Ayers <bradley.ayers@gmail.com>
2025-06-16 13:52:51 +1000

    fix vercel.json

--- projects/app/src/client/ui/postHogOptions.ts
+++ projects/app/src/client/ui/postHogOptions.ts
@@ -1,3 +1,5 @@
-export const debug = __DEV__;
+// Set this to true to turn on debug console logs for PostHog. They output via
+// `console.log()` so you can see them in the browser console.
+export const debug = false;
 export const apiKey = process.env.EXPO_PUBLIC_POSTHOG_API_KEY;
 export const apiHost = `https://eu.i.posthog.com`;
