53bb5e24124912f6eaa27fc2006d0d5408da626e
Bradley Ayers <bradley.ayers@gmail.com>
2025-06-24 21:09:10 +1000

    increase vitest timeout to 15s

--- projects/app/vitest.config.ts
+++ projects/app/vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     fakeTimers: {
       now: 0,
     },
-    testTimeout: 10_000, // pglite can be slow
+    testTimeout: 15_000, // pglite can be slow
   },
   plugins: [tsconfigPaths(), react()],
 });
