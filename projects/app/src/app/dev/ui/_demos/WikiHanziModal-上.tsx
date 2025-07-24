2a25810df51b72beeac2247e0fd2faf9abb43f58
Bradley Ayers <bradley.ayers@gmail.com>
2025-07-24 14:12:18 +1000

    fix /dev/ui mobile width for WikiHanziModal

--- projects/app/src/app/dev/ui/_demos/WikiHanziModal-上.tsx
+++ projects/app/src/app/dev/ui/_demos/WikiHanziModal-上.tsx
@@ -1,15 +1,12 @@
 import { WikiHanziModal } from "@/client/ui/WikiHanziModal";
 import type { HanziText } from "@/data/model";
-import { View } from "react-native";
 
 export default () => {
   return (
-    <View>
-      <WikiHanziModal
-        devUiSnapshotMode
-        hanzi={`上` as HanziText}
-        onDismiss={() => null}
-      />
-    </View>
+    <WikiHanziModal
+      devUiSnapshotMode
+      hanzi={`上` as HanziText}
+      onDismiss={() => null}
+    />
   );
 };
--- projects/app/src/app/dev/ui/_demos/WikiHanziWordModal.tsx
+++ projects/app/src/app/dev/ui/_demos/WikiHanziWordModal.tsx
@@ -1,16 +1,11 @@
 import { WikiHanziWordModal } from "@/client/ui/WikiHanziWordModal";
-import { View } from "react-native";
 
 export default () => {
   return (
-    <>
-      <View>
-        <WikiHanziWordModal
-          devUiSnapshotMode
-          hanziWord={`你好:hello`}
-          onDismiss={() => null}
-        />
-      </View>
-    </>
+    <WikiHanziWordModal
+      devUiSnapshotMode
+      hanziWord={`你好:hello`}
+      onDismiss={() => null}
+    />
   );
 };
