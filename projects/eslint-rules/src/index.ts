a538baa6fb8739c9afdcf9cdd91e5dff24a37b0e
Bradley Ayers <bradley.ayers@gmail.com>
2026-02-06 11:37:55 +1100

    remove logical-assignment-operators eslint rule

--- projects/eslint-rules/src/index.ts
+++ projects/eslint-rules/src/index.ts
@@ -53,7 +53,6 @@ const recommended: ConfigWithExtendsArray = [
       // eslint-base
       //
 
-      "logical-assignment-operators": `error`,
       "no-process-exit": `error`,
       "one-var": [`error`, `never`],
 
