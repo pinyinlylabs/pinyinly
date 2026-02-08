6b940eedb3f983d66851e5df047178b2a8e1412a
Bradley Ayers <bradley.ayers@gmail.com>
2026-02-08 11:19:00 +1100

    fix: remove unnecessary comment disabling linting for relative parent imports

--- projects/app/bin/generateMnemonicTheme.ts
+++ projects/app/bin/generateMnemonicTheme.ts
@@ -24,7 +24,6 @@ import makeDebug from "debug";
 import path from "node:path";
 import yargs from "yargs";
 import { z } from "zod/v4";
-// oxlint-disable-next-line import/no-relative-parent-imports
 import { dataDir } from "../test/helpers.ts";
 import { makeSimpleAiClient } from "./util/openai.js";
 
