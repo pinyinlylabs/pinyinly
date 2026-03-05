154b9ef99c210fa54390a4f9222377e5301b71e1
Bradley Ayers <bradley.ayers@gmail.com>
2026-03-05 12:55:55 +0800

    feat: enhance SettingText component to support fallback values and improve type safety

--- projects/app/src/client/ui/SettingText.tsx
+++ projects/app/src/client/ui/SettingText.tsx
@@ -4,19 +4,28 @@ import type {
 } from "@/client/ui/hooks/useUserSetting";
 import { useUserSetting } from "@/client/ui/hooks/useUserSetting";
 import type { UserSettingTextEntity } from "@/data/userSettings";
+import type { IsExhaustedRest } from "@pinyinly/lib/types";
 import { useUserSettingTextDefaultValue } from "./hooks/useUserSettingTextDefaultValue";
 
 interface SettingTextProps<T extends UserSettingTextEntity> {
   setting: UserSettingEntityLike<T>;
   settingKey: UserSettingKeyInput<T>;
+  fallback?: string;
 }
 
 export function SettingText<T extends UserSettingTextEntity>({
   setting,
   settingKey,
-}: SettingTextProps<T>): string {
-  const { value } = useUserSetting(setting, settingKey);
+  fallback,
+  ...rest
+}: SettingTextProps<T>): string | null {
+  true satisfies IsExhaustedRest<typeof rest>;
+
+  let { value } = useUserSetting(setting, settingKey);
+  if (value?.text.length === 0) {
+    value = null;
+  }
   const defaultValue = useUserSettingTextDefaultValue(setting, settingKey);
 
-  return value?.text ?? defaultValue ?? ``;
+  return value?.text ?? defaultValue ?? fallback ?? null;
 }
